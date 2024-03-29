9498d035f183170d700b6125ed7be27c703f58f8
Arne Seime <arne.seime@gmail.com>
2024-03-29 19:05:46 +0100

    Inlined noise-java

---
+++ src/test/java/com/southernstorm/noise/tests/UnitVectorTests.java
@@ -0,0 +1,32 @@
+package com.southernstorm.noise.tests;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UnitVectorTests {
+
+    private static final String testVectorsCommit = "5d0a74760320e5486ced302e36ccad91606aac43";
+
+    @Test
+    public void testBasicVector() throws Exception {
+        try (InputStream stream = new URL("https://raw.githubusercontent.com/rweather/noise-c/" + testVectorsCommit
+                + "/tests/vector/noise-c-basic.txt").openStream()) {
+            VectorTests vectorTests = new VectorTests();
+            vectorTests.processInputStream(stream);
+            Assert.assertEquals(0, vectorTests.getFailed());
+        }
+    }
+
+    @Test
+    public void testCacophonyVector() throws Exception {
+        try (InputStream stream = new URL(
+                "https://raw.githubusercontent.com/centromere/cacophony/master/vectors/cacophony.txt").openStream()) {
+            VectorTests vectorTests = new VectorTests();
+            vectorTests.processInputStream(stream);
+            Assert.assertEquals(0, vectorTests.getFailed());
+        }
+    }
+}
