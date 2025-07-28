8cc73d2cffea5e0d5b0b38339e16e21ce872d40d
Arne Seime <arne.seime@gmail.com>
2025-07-28 09:28:12 +0200

    Chore: Reduce string duplications

--- src/main/java/no/seime/openhab/binding/esphome/internal/message/SensorNumberDeviceClass.java
+++ src/main/java/no/seime/openhab/binding/esphome/internal/message/SensorNumberDeviceClass.java
@@ -7,30 +7,30 @@ public enum SensorNumberDeviceClass {
     GENERIC_NUMBER("generic_number", NUMBER, "zoom", null),
     ENUM("enum", STRING, "text", null),
     TIMESTAMP("timestamp", DATETIME, "time", null),
-    APPARENT_POWER("apparent_power", "Number:Power", "energy", null),
+    APPARENT_POWER("apparent_power", toItem("Power"), "energy", null),
     AQI("aqi", NUMBER, "smoke", null),
-    ATMOSPHERIC_PRESSURE("atmospheric_pressure", "Number:Pressure", "pressure", null),
-    BATTERY("battery", "Number:Dimensionless", "batterylevel", null),
-    CO("carbon_monoxide", "Number:Dimensionless", "smoke", "CO"),
-    CO2("carbon_dioxide", "Number:Dimensionless", "carbondioxide", "CO2"),
-    CURRENT("current", "Number:ElectricCurrent", "energy", "Current"),
+    ATMOSPHERIC_PRESSURE("atmospheric_pressure", toItem("Pressure"), "pressure", null),
+    BATTERY("battery", toItem("Dimensionless"), "batterylevel", null),
+    CO("carbon_monoxide", toItem("Dimensionless"), "smoke", "CO"),
+    CO2("carbon_dioxide", toItem("Dimensionless"), "carbondioxide", "CO2"),
+    CURRENT("current", toItem("ElectricCurrent"), "energy", "Current"),
 
-    DATA_RATE("data_rate", "Number:DataTransferRate", null, null),
-    DATA_SIZE("data_size", "Number:DataAmount", null, null),
-    DISTANCE("distance", "Number:Length", null, null),
-    DURATION("duration", "Number:Time", "time", null),
-    ENERGY("energy", "Number:Energy", "energy", "Energy"),
-    ENERGY_STORAGE("energy_storage", "Number:Energy", "energy", "Energy"),
-    FREQUENCY("frequency", "Number:Frequency", null, "Frequency"),
+    DATA_RATE("data_rate", toItem("DataTransferRate"), null, null),
+    DATA_SIZE("data_size", toItem("DataAmount"), null, null),
+    DISTANCE("distance", toItem("Length"), null, null),
+    DURATION("duration", toItem("Time"), "time", null),
+    ENERGY("energy", toItem("Energy"), "energy", "Energy"),
+    ENERGY_STORAGE("energy_storage", toItem("Energy"), "energy", "Energy"),
+    FREQUENCY("frequency", toItem("Frequency"), null, "Frequency"),
 
-    GAS("gas", "Number:Volume", "gas", "Gas"),
-    HUMIDITY("humidity", "Number:Dimensionless", "humidity", "Humidity"),
-    ILLUMINANCE("illuminance", "Number:Illuminance", "lightbulb", "Light"),
-    IRRADIANCE("irradiance", "Number:Itensity", null, null),
-    MOISTURE("moisture", "Number:Dimensionless", "water", "Humidity"),
+    GAS("gas", toItem("Volume"), "gas", "Gas"),
+    HUMIDITY("humidity", toItem("Dimensionless"), "humidity", "Humidity"),
+    ILLUMINANCE("illuminance", toItem("Illuminance"), "lightbulb", "Light"),
+    IRRADIANCE("irradiance", toItem("Intensity"), null, null),
+    MOISTURE("moisture", toItem("Dimensionless"), "water", "Humidity"),
     MONETARY("monetary", NUMBER, null, null), // TODO: Add Monetary type
                                               // https://github.com/openhab/openhab-core/issues/3408
-    NITROGEN_DIOXIDE("nitrogen_dioxide", "Number:Dimensionless", "smoke", "null"),
+    NITROGEN_DIOXIDE("nitrogen_dioxide", toItem("Dimensionless"), "smoke", "null"),
     NITROGEN_MONOXIDE("nitrogen_monoxide", NUMBER, "smoke", null),
     NITROUS_OXIDE("nitrous_oxide", NUMBER, "smoke", null),
 
@@ -39,29 +39,32 @@ public enum SensorNumberDeviceClass {
     PM1("pm1", NUMBER, "smoke", null),
     PM10("pm10", NUMBER, "smoke", null),
     PM25("pm25", NUMBER, "smoke", null),
-    POWER_FACTOR("power_factor", "Number:Dimensionless", "energy", "Power"),
-    POWER("power", "Number:Power", "energy", "Power"),
-    PRECIPITATION("precipitation", "Number:Length", "rain", "Rain"),
-    PRECIPITATION_RATE("precipitation_rate", "Number:Speed", "rain", "Rain"),
-    PRESSURE("pressure", "Number:Pressure", "pressure", "Pressure"),
-    REACTIVE_POWER("reactive_power", "Number:Power", "energy", "Power"),
-    SIGNAL_STRENGTH("signal_strength", "Number:Power", "qualityofservice", null),
+    POWER_FACTOR("power_factor", toItem("Dimensionless"), "energy", "Power"),
+    POWER("power", toItem("Power"), "energy", "Power"),
+    PRECIPITATION("precipitation", toItem("Length"), "rain", "Rain"),
+    PRECIPITATION_RATE("precipitation_rate", toItem("Speed"), "rain", "Rain"),
+    PRESSURE("pressure", toItem("Pressure"), "pressure", "Pressure"),
+    REACTIVE_POWER("reactive_power", toItem("Power"), "energy", "Power"),
+    SIGNAL_STRENGTH("signal_strength", toItem("Power"), "qualityofservice", null),
 
-    SOUND_PRESSURE("sound_pressure", "Number:Dimensionless", "soundvolume", "SoundVolume"),
-    SPEED("speed", "Number:Speed", "motion", null),
+    SOUND_PRESSURE("sound_pressure", toItem("Dimensionless"), "soundvolume", "SoundVolume"),
+    SPEED("speed", toItem("Speed"), "motion", null),
     SULPHUR_DIOXIDE("sulphur_dioxide", NUMBER, "smoke", null),
-    TEMPERATURE("temperature", "Number:Temperature", "temperature", "Temperature"),
+    TEMPERATURE("temperature", toItem("Temperature"), "temperature", "Temperature"),
     VOLATILE_ORGANIC_COMPOUNDS("volatile_organic_compounds", NUMBER, "smoke", null),
-    VOLATILE_ORGANIC_COMPOUNDS_PARTS("volatile_organic_compounds_parts", "Number:Dimensionless", "smoke", null),
-    VOLTAGE("voltage", "Number:ElectricPotential", "energy", "Voltage"),
-    VOLUME("volume", "Number:Volume", null, null),
-    VOLUME_STORAGE("volume_storage", "Number:Volume", null, null),
-    WATER("water", "Number:Volume", "water", "Water"),
-    WEIGHT("weight", "Number:Force", null, null),
-    WIND_SPEED("wind_speed", "Number:Speed", "wind", "Wind");
+    VOLATILE_ORGANIC_COMPOUNDS_PARTS("volatile_organic_compounds_parts", toItem("Dimensionless"), "smoke", null),
+    VOLTAGE("voltage", toItem("ElectricPotential"), "energy", "Voltage"),
+    VOLUME("volume", toItem("Volume"), null, null),
+    VOLUME_STORAGE("volume_storage", toItem("Volume"), null, null),
+    WATER("water", toItem("Volume"), "water", "Water"),
+    WEIGHT("weight", toItem("Force"), null, null),
+    WIND_SPEED("wind_speed", toItem("Speed"), "wind", "Wind");
+
+    private static String toItem(String dimension) {
+        return String.format("%s:%s", NUMBER, dimension);
+    }
 
     private final String deviceClass;
-
     private final String itemType;
     private final String category;
     private final String semanticType;
