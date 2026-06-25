15fcea323890c2f43aefbe47561d232c58abb0bd
Arne Seime <arne.seime@gmail.com>
2026-06-25 18:07:03 +0200

    Improve ThingAction testcase

--- src/test/java/no/seime/openhab/binding/esphome/devicetest/ThingActionEsphomeDeviceTest.java
+++ src/test/java/no/seime/openhab/binding/esphome/devicetest/ThingActionEsphomeDeviceTest.java
@@ -2,14 +2,15 @@ package no.seime.openhab.binding.esphome.devicetest;
 
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 import java.io.File;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
+import org.mockito.ArgumentCaptor;
+import org.openhab.core.thing.binding.ThingActions;
 import org.osgi.framework.ServiceRegistration;
 
 public class ThingActionEsphomeDeviceTest extends AbstractESPHomeDeviceTest {
@@ -21,13 +22,25 @@ public class ThingActionEsphomeDeviceTest extends AbstractESPHomeDeviceTest {
     @Test
     public void testThingAction() {
 
-        doReturn(mock(ServiceRegistration.class)).when(bundleContext).registerService(anyString(),
-                ArgumentMatchers.any(), ArgumentMatchers.any());
+        doReturn(mock(ServiceRegistration.class)).when(bundleContext).registerService(eq(ThingActions.class),
+                any(ThingActions.class), any());
 
         thingHandler.initialize();
         await().until(() -> thingHandler.isInterrogated());
 
         assertEquals(2, thingHandler.getDynamicChannels().size());
+
+        ArgumentCaptor<ThingActions> captor = ArgumentCaptor.forClass(ThingActions.class);
+        verify(bundleContext, times(3)).registerService(eq(ThingActions.class), captor.capture(), any());
+
+        List<ThingActions> capturedActions = captor.getAllValues();
+
+        assertTrue(capturedActions.stream().anyMatch(a -> a.getClass().getName()
+                .equals("no.seime.openhab.binding.esphome.internal.handler.action.FirmwareUpgradeAction")));
+        assertTrue(capturedActions.stream().anyMatch(a -> a.getClass().getName()
+                .equals("no.seime.openhab.binding.esphome.internal.handler.action.SnakeCaseAction")));
+        assertTrue(capturedActions.stream().anyMatch(a -> a.getClass().getName()
+                .equals("no.seime.openhab.binding.esphome.internal.handler.action.SimpleAction")));
 
         thingHandler.dispose();
     }
